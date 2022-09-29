14e725dae7b44776f2067b58f2b0ca93882448d0
Yohovani <yohovanivargas@gmail.com>
2022-09-28 19:03:17 -0500

    Section 10 RNN Part 2 Building the RNN

--- RNN.py
+++ RNN.py
@@ -9,7 +9,6 @@ Created on Tue Sep 27 16:52:18 2022
 # Recurrent Neural Network
 
 
-
 # Part 1 - Data Preprocessing
 
 # Importing the libraries
@@ -22,12 +21,12 @@ import pandas as pd
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:, 1:2].values
 
-#Feature Scaling
+# Feature Scaling
 from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range= (0,1))
 training_set_scaled = sc.fit_transform(training_set)
 
-#Creating a data structure with 60 timesteps and 1 output
+# Creating a data structure with 60 timesteps and 1 output
 
 x_train = []
 y_train = []
@@ -36,9 +35,43 @@ for i in range(60, 1258):
     y_train.append(training_set_scaled[i, 0])
 x_train, y_train = np.array(x_train), np.array(y_train)
 
-#Reshaping 
-
+# Reshaping 
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 # Part 2 - Building the RNN
+# Import the keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+# Initialising the RNN
+regressor = Sequential()
+
+# Adding the first LSTM layer and some Dropout Regularisation
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (x_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+# Adding the second LSTM layer and some Dropout Regularisation
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+# Adding the third LSTM layer and some Dropout Regularisation
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+# Adding the fourth LSTM layer and some Dropout Regularisation
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.2))
+
+# Adding the output layer
+regressor.add(Dense(units = 1))
+
+# Compiling the RNN
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+# Fitting the RNN to the Training set
+regressor.fit(x_train, y_train, epochs=100, batch_size=32)
+
 
 # Part 3 - Making the predictions and visualising the results
